edbf90dabc58fa1ebe682d05eebe1f881a87696e
Ryan <ryanpun282@gmail.com>
2020-01-31 20:12:45 -0800

    0.1

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const path = require('path')
-const port = 3000
+const path = require('path');
+const port = 3000;
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
 
-app.listen(port, _ => console.log('listening'))
+app.listen(port, _ => console.log('listening'));
