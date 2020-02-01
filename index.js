6c8fb91995e8e558d69361961f2ecd242509cc3d
Ryan <ryanpun282@gmail.com>
2020-01-31 20:23:56 -0800

    0.1

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(__dirname));
-app set('port', (process.env.PORT || 5000));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`App is running on port ${ port }`);
+});
 
 app.listen(port, _ => console.log('listening'));
