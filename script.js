c465609362b696a7af2014d8b14c30894811a689
Ryan <ryanpun282@gmail.com>
2020-01-31 23:29:17 -0800

    0.1

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function fbSDKLoaded() {
 	FB.getLoginStatus(response => {
 		if (response.status == "not_authorized") {
 			loginWithFacebook = _ => {
+				console.log('test');
 				FB.login(response => {
 					console.log(response)
 				})
