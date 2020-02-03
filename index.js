23a3071a211df71be3b8f3bcc2eac6128f29df98
Ryan <ryanpun282@gmail.com>
2020-02-02 21:49:10 -0800

    v0.1

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.redirect(path.join(req, '/home'));
+});
+
 app.listen(port, () => {
     const d = new Date().getTime();
     console.log(`${new Date(d).toString()} App is running on port ${ port }`);
